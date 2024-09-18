66a6c3bb5cdd9baa55d755142ca3de2f2a28d21f
DressaCarvalho <andressamdc18@gmail.com>
2024-09-18 19:55:25 -0300

    Dia 10

---
+++ Dia 10/dia10.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+#exemplo de criação de dataframe
+data = {
+    'nome' : ['Ana', 'João', 'Maria', 'Pedro', 'Lucas', 'Carla'],
+    'departamento' : ['TI', 'RH', 'TI', 'Financeiro', 'TI', 'RH'],
+    'salario' : [5000, 4500,5200, 6000, 4800, 4700]
+}
+df = pd.DataFrame(data)
+#filtragem dos funcionarios de TI
+funcionarios_ti = df[df['departamento'] == 'TI']
+print(funcionarios_ti)
+# media salarial
+media_salarial = funcionarios_ti['salario'].mean()
+#exibindo resultados
+print(f"Média salarial dos funcionarios de TI: {media_salarial}")
